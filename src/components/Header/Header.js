148deacab6b755358284dc7393cb084622681eea
AlejandroV22 <alejandrovelandiagelvez@gmail.com>
2024-10-26 15:38:44 -0500

    primer cambio

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+// components/Header/Header.js
+import React from 'react';
+import '../../styles/Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="/src/images/capibara.jpg" alt="Logo" />
+      </div>
+      <h1 className="title">Gestión de taller mecánico</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/scripts/main.js
@@ -0,0 +1,10 @@
+// scripts/main.js
+
+// Función para inicializar el comportamiento del encabezado
+function initHeader() {
+  // Lógica de inicialización del encabezado
+  console.log('Header initialized');
+}
+
+// Ejecutar la función cuando el DOM esté listo
+document.addEventListener('DOMContentLoaded', initHeader);
