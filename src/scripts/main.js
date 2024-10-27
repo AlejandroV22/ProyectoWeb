7d710b1ed283449c623eab28e8cd9626408d5e6a
Farid Rojas <111253744+FaridRojas@users.noreply.github.com>
2024-10-27 11:16:56 -0500

    Delete src directory

--- src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-// src/scripts/main.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App'; // Importa el componente principal App
-
-// Renderiza la aplicación en el elemento con id "root"
-ReactDOM.render(<App />, document.getElementById('root'));
-
